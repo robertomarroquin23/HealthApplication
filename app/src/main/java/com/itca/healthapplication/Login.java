73d19078323bf873e1609e8b3745a734ee103340
ArielCarbajal <127555788+ArielCarbajal@users.noreply.github.com>
2024-04-27 12:02:25 -0600

    Pantalla del menu finalizada, unicamente la redireccion del menu no es la correcta ya que no tenemos mas activities

--- app/src/main/java/com/itca/healthapplication/Login.java
+++ app/src/main/java/com/itca/healthapplication/Login.java
@@ -1,6 +1,9 @@
 package com.itca.healthapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Login extends AppCompatActivity {
 
+    Button btConfirmar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btConfirmar = findViewById(R.id.btConfirmar);
+
+        btConfirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/itca/healthapplication/MainActivity.java
+++ app/src/main/java/com/itca/healthapplication/MainActivity.java
@@ -2,13 +2,28 @@ package com.itca.healthapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView salud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        salud = findViewById(R.id.ivSalud);
+
+        salud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class );
+                startActivity(intent);
+            }
+        });
     }
 }
