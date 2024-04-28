f444c282362eff0d46910b846a9702972ef79830
Roberto Francisco Marroquín Osorio <67878026+robertomarroquin1313@users.noreply.github.com>
2024-04-28 11:35:27 -0600

    Commit de prueba

---
+++ app/src/main/java/com/itca/healthapplication/View/MainView/ExerciseTrainingLog.java
@@ -0,0 +1,21 @@
+package com.itca.healthapplication.View.MainView;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.itca.healthapplication.R;
+
+public class ExerciseTrainingLog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_training_log);
+
+    }
+}
